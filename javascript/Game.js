059cc44581c7df868a709d62868eaccb69738dfa
Jurgita <jurgita.eicinaite@yahoo.com>
2024-02-29 10:32:04 +0200

    Game and Player classes

---
+++ javascript/Game.js
@@ -0,0 +1,17 @@
+import Player from './Player.js';
+
+class Game {
+    constructor(canvas, context) {
+        this.canvas = canvas;
+        this.ctx = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.player = new Player(this);
+    }
+    render() {
+        this.ctx.fillStyle = 'red';
+        this.player.draw();
+    }
+}
+
+export default Game;
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('helo world');
+import Game from './javascript/Game.js';
+console.log('hello');
+
+window.addEventListener('load', function () {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 720;
+    canvas.height = 720;
+
+    const game = new Game(canvas, ctx);
+
+    game.render();
+});
