993745f9b154b2097337d3b1bf90ebbb016e9828
Jurgita <jurgita.eicinaite@yahoo.com>
2024-02-29 11:21:35 +0200

    resizing canvas functionality

--- javascript/Game.js
+++ javascript/Game.js
@@ -7,9 +7,20 @@ class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
+        this.resize(window.innerWidth, window.innerHeight);
+
+        window.addEventListener('resize', e => {
+            this.resize(e.currentTarget.innerWidth, e.currentTarget.innerHeight);
+        });
+    }
+    resize(width, height) {
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.ctx.fillStyle = 'red';
+        this.height = this.canvas.height;
+        this.width = this.canvas.width;
     }
     render() {
-        this.ctx.fillStyle = 'red';
         this.player.update();
         this.player.draw();
     }
