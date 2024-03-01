068eea662b21ddc6f0370b190921649d1e16f622
Jurgita <jurgita.eicinaite@yahoo.com>
2024-03-01 09:48:19 +0200

    endless repeating background

--- javascript/Background.js
+++ javascript/Background.js
@@ -2,21 +2,34 @@ class Background {
     constructor(game) {
         this.game = game;
         this.image = document.getElementById('background');
+        this.image2 = document.getElementById('background2');
         this.width = 2400;
         this.height = this.game.baseHeight;
+        this.scaledWidth;
+        this.scaledHeight;
         this.x;
     }
 
     update() {
         this.x -= this.game.speed;
-        if (this.x + this.width <= 0) this.x = 0;
+        if (this.x + this.scaledWidth <= 0) this.x = 0;
     }
 
     draw() {
-        this.game.ctx.drawImage(this.image, this.x, 0);
+        this.game.ctx.drawImage(this.image, this.x, 0, this.scaledWidth, this.scaledHeight);
+        let lastX = this.x;
+        let calc = 1;
+        // How many backgrounds to draw
+        while (this.game.width >= this.scaledWidth + lastX) {
+            lastX += this.scaledWidth;
+            this.game.ctx.drawImage(this.image, lastX, 0, this.scaledWidth, this.scaledHeight);
+            calc++;
+        }
     }
 
     resize() {
+        this.scaledWidth = this.width * this.game.ratio;
+        this.scaledHeight = this.height * this.game.ratio;
         this.x = 0;
     }
 }
