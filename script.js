b3dabb8bdaf0af33d6fb49a646e53905d8f06c36
Jurgita <jurgita.eicinaite@yahoo.com>
2024-03-09 17:48:51 +0200

    cleanup scipt.js, export launch logic to gameLaunch.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-import Game from './javascript/Game.js';
+import gameLaunch from './javascript/gameLaunch.js';
 
-window.addEventListener('load', function () {
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 720;
-    canvas.height = 720;
-
-    const game = new Game(canvas, ctx);
-
-    let lastTime = 0;
-
-    // implementation of adjusted fps
-    let fpsAdjustment = 0;
-
-    function animate(timeStamp) {
-        const deltaTime = timeStamp - lastTime;
-        lastTime = timeStamp;
-        fpsAdjustment += deltaTime;
-
-        // if condition for fps adjustment 60fps
-        if (fpsAdjustment >= 16.67 && !game.isPaused) {
-            // ctx.clearRect(0, 0, canvas.width, canvas.height);
-            game.render(deltaTime);
-            fpsAdjustment = fpsAdjustment % 16.67;
-        }
-        requestAnimationFrame(animate);
-    }
-
-    requestAnimationFrame(animate);
-});
+gameLaunch();
